0c13b6686c9c8ea76db96adaead429c5177361e3
Raí Campos <raicampas23@gmail.com>
2020-11-25 13:04:31 -0500

    creación de vista de myBookings

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace A_Rent_demo.Controllers
             return View();
         }
 
+        public IActionResult Bookings()
+        {
+            return View();
+        }
+
 
         public IActionResult Privacy()
         {
